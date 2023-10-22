8eb507ca3ca0ca9c36238f25cc663285cc5d694f
Yashwanth Rao Gujja <yg7bh@umsystem.edu>
2023-10-22 11:14:44 -0500

    1NF Complete

--- input_parser.py
+++ input_parser.py
@@ -1,1 +1,15 @@
 # This file is used to parse the input file and dependencies
+def contains_comma(series):
+    return series.str.contains(',').any()
+
+
+def input_parser(input_file):
+    input_file = input_file.astype(str)
+    columns_with_commas = [
+        col for col in input_file.columns if contains_comma(input_file[col])]
+
+    for col in columns_with_commas:
+        input_file[col] = input_file[col].str.split(
+            ',').apply(lambda x: [item.strip() for item in x])
+
+    return input_file
--- main.py
+++ main.py
@@ -2,11 +2,7 @@
 import pandas as pd
 import csv
 import normalizations
-
-
-def contains_comma(series):
-    """Check if a pandas Series contains comma-separated values."""
-    return series.str.contains(',').any()
+import input_parser
 
 
 # Reading the input csv file and the dependencies text file
@@ -15,17 +11,14 @@ with open('dependencies.txt', 'r') as file:
     input_dependencies = file.read()
 
 # Input from the user
-max_normalization = input(
-    'Please enter the highest desired level of normalization: ')
+max_normalization = int(input(
+    'Please enter the highest desired level of normalization: '))
 
-input_file = input_file.astype(str)
-columns_with_commas = [
-    col for col in input_file.columns if contains_comma(input_file[col])]
-print(columns_with_commas)
-for col in columns_with_commas:
-    input_file[col] = input_file[col].str.split(
-        ',').apply(lambda x: [item.strip() for item in x])
+input_file = input_parser.input_parser(input_file)
 
-one_nf_table, one_flag = normalizations.first_normalization_form(input_file)
-print(one_nf_table)
-print(one_flag)
+if max_normalization <= 1:
+    one_nf_table, one_flag = normalizations.first_normalization_form(
+        input_file)
+
+    if one_flag and max_normalization == 1:
+        print('Already Normalized to 1NF')
